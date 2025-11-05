4950b993dc7cc9907adda2cac49f04e70acf021b
enpitsulin <enpitsulin@gmail.com>
2025-11-05 17:07:34 +0800

    test: 去除 singleThread 设置

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     testTimeout: 30 * 1000,
-    poolOptions: {
-      threads: {
-        singleThread: true,
-      },
-    },
   },
 })
